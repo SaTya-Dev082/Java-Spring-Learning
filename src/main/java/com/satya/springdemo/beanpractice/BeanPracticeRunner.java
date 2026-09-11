51cfc34fe1488c465e29fdd8b55b4570c0f4c34f
SaTya-Dev082 <soeunnoraksatya@gmail.com>
2026-09-11 10:00:52 +0700

    apply concept section bean with real example

---
+++ src/main/java/com/satya/springdemo/beanpractice/BeanPracticeRunner.java
@@ -0,0 +1,24 @@
+package com.satya.springdemo.beanpractice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component 
+public class BeanPracticeRunner implements CommandLineRunner {
+    private final ApplicationContext context;
+
+    public BeanPracticeRunner(ApplicationContext context) {
+        this.context = context;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("===== BEAN PRACTICE =====");
+
+        ProductService productService = context.getBean(ProductService.class);
+
+        productService.purchaseProduct();
+    }
+
+}
